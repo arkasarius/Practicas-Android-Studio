3c1a3dd9d82065e1aae2419aee94c7f93268c940
arkasarius <roger.ordonez.brumos@gmail.com>
2018-02-18 16:46:53 +0100

    Hasmap buttonIDStoVal <String,Integer> --> Hasmap buttonIDStoVal <String,Character> aixi la sequencia de carcters no s'ha de transformar de int a caracter i estalviem un pas. Funcionalitat afegida --> s'incorporan numeros al textview al polsar els botons.
    APP finalitzada.

--- app1/app/src/main/java/com/example/roger/fakephone/Call_activity.java
+++ app1/app/src/main/java/com/example/roger/fakephone/Call_activity.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 public class Call_activity extends AppCompatActivity {
 
 
-    HashMap<String, Integer> buttonIDStoVal=new HashMap<String, Integer>();
+    HashMap<String, Character> buttonIDStoVal=new HashMap<String, Character>();
     TextView phoneNumber;
 
     @Override
@@ -20,24 +20,24 @@ public class Call_activity extends AppCompatActivity {
         setContentView(R.layout.activity_call_activity);
         //
         //hashMap values
-        buttonIDStoVal.put("btn_0",0);
-        buttonIDStoVal.put("btn_1",1);
-        buttonIDStoVal.put("btn_2",2);
-        buttonIDStoVal.put("btn_3",3);
-        buttonIDStoVal.put("btn_4",4);
-        buttonIDStoVal.put("btn_5",5);
-        buttonIDStoVal.put("btn_6",6);
-        buttonIDStoVal.put("btn_7",7);
-        buttonIDStoVal.put("btn_8",8);
-        buttonIDStoVal.put("btn_9",9);
+        buttonIDStoVal.put("btn_0",'0');
+        buttonIDStoVal.put("btn_1",'1');
+        buttonIDStoVal.put("btn_2",'2');
+        buttonIDStoVal.put("btn_3",'3');
+        buttonIDStoVal.put("btn_4",'4');
+        buttonIDStoVal.put("btn_5",'5');
+        buttonIDStoVal.put("btn_6",'6');
+        buttonIDStoVal.put("btn_7",'7');
+        buttonIDStoVal.put("btn_8",'8');
+        buttonIDStoVal.put("btn_9",'9');
         //phone textview set
         phoneNumber=findViewById(R.id.PhoneNumberID);
 
     }
     public void butonPress(View v){
         String id =v.getResources().getResourceEntryName(v.getId());
-        int value=buttonIDStoVal.get(id);
-        Toast.makeText(this, Integer.toString(value), Toast.LENGTH_SHORT).show();
+        char value=buttonIDStoVal.get(id);
+        phoneNumber.setText(phoneNumber.getText().toString()+value);
     }
 
     public void EsborraNum(View view) {
@@ -47,7 +47,7 @@ public class Call_activity extends AppCompatActivity {
     public void TrucaNum(View view) {
         String phoneNUM = phoneNumber.getText().toString();
         if(!phoneNUM.isEmpty()){
-            Toast.makeText(this, "Teucant al "+phoneNUM, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Trucant al "+phoneNUM, Toast.LENGTH_SHORT).show();
         }else{
             Toast.makeText(this, "No hi ha cap numero marcat", Toast.LENGTH_SHORT).show();
         }
