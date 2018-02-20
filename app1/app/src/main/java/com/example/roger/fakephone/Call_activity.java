424265576b2cc9cd30ec7fe631882deb8556047e
arkasarius <roger.ordonez.brumos@gmail.com>
2018-02-20 11:21:53 +0100

    APP revisió V2. Capturem el View com a button (aixo pot petar si el view no es un button). agafem el text del botó i el fem servir com a recurs i treiem la necesitat del hashmap.

--- app1/app/src/main/java/com/example/roger/fakephone/Call_activity.java
+++ app1/app/src/main/java/com/example/roger/fakephone/Call_activity.java
@@ -3,6 +3,7 @@ package com.example.roger.fakephone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,34 +11,20 @@ import java.util.HashMap;
 
 public class Call_activity extends AppCompatActivity {
 
-
-    HashMap<String, Character> buttonIDStoVal=new HashMap<String, Character>();
     TextView phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_activity);
-        //
-        //hashMap values
-        buttonIDStoVal.put("btn_0",'0');
-        buttonIDStoVal.put("btn_1",'1');
-        buttonIDStoVal.put("btn_2",'2');
-        buttonIDStoVal.put("btn_3",'3');
-        buttonIDStoVal.put("btn_4",'4');
-        buttonIDStoVal.put("btn_5",'5');
-        buttonIDStoVal.put("btn_6",'6');
-        buttonIDStoVal.put("btn_7",'7');
-        buttonIDStoVal.put("btn_8",'8');
-        buttonIDStoVal.put("btn_9",'9');
+
         //phone textview set
         phoneNumber=findViewById(R.id.PhoneNumberID);
 
     }
     public void butonPress(View v){
-        String id =v.getResources().getResourceEntryName(v.getId());
-        char value=buttonIDStoVal.get(id);
-        phoneNumber.setText(phoneNumber.getText().toString()+value);
+        Button b = (Button)v;
+        phoneNumber.setText(phoneNumber.getText().toString()+b.getText());
     }
 
     public void EsborraNum(View view) {
