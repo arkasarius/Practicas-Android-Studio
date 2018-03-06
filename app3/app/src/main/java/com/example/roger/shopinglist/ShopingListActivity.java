71d8065e0653f35a6fe71f88337f523f04929435
arkasarius <roger.ordonez.brumos@gmail.com>
2018-03-06 10:35:25 +0100

    layout

---
+++ app3/app/src/main/java/com/example/roger/shopinglist/ShopingListActivity.java
@@ -0,0 +1,13 @@
+package com.example.roger.shopinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoping_list);
+    }
+}
