9c5ed772f7063bd83a71565e5452f3cc26ee836e
arkasarius <roger.ordonez.brumos@gmail.com>
2018-03-06 11:54:13 +0100

    shoping list functionalities i quadre de dialeg i esborrar

--- app3/app/src/main/java/com/example/roger/shopinglist/ShopingListActivity.java
+++ app3/app/src/main/java/com/example/roger/shopinglist/ShopingListActivity.java
@@ -1,13 +1,17 @@
 package com.example.roger.shopinglist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class ShopingListActivity extends AppCompatActivity {
 
@@ -44,16 +48,31 @@ public class ShopingListActivity extends AppCompatActivity {
         });
     }
 
-    private void onRemoveItem(int position) {
-        items.remove(position);
-        adapter.notifyDataSetChanged();
+    private void onRemoveItem(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        builder.setMessage(String.format(Locale.getDefault(),"Estàs segur que vols esborrar '%s'",items.get(position)));
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                items.remove(position);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel,null);
+        builder.create().show();
+
     }
 
     public void onAddItem(View view) {
         String item = new_item.getText().toString();
-        items.add(item);
-        adapter.notifyDataSetChanged();
-        new_item.setText("");
-        list.smoothScrollToPosition(items.size()-1);
+        if(item.isEmpty()){
+            Toast.makeText(this, "No has escrit res!", Toast.LENGTH_SHORT).show();
+        }else{
+         items.add(item);
+            adapter.notifyDataSetChanged();
+            new_item.setText("");
+            list.smoothScrollToPosition(items.size()-1);
+        }
     }
 }
